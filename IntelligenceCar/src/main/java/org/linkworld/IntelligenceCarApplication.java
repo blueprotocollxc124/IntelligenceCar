a12aaf41b2984c88e8bf2db6346008158017d5f7
Blue ProtocolLXC <81018806+blueprotocollxc124@users.noreply.github.com>
2022-03-05 18:45:28 +0800

    Add files via upload

---
+++ IntelligenceCar/src/main/java/org/linkworld/IntelligenceCarApplication.java
@@ -0,0 +1,13 @@
+package org.linkworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligenceCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligenceCarApplication.class, args);
+    }
+
+}
---
+++ IntelligenceCar/src/test/java/org/linkworld/IntelligenceCarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.linkworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligenceCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
